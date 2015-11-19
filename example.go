d3ed849ddfe789948370b32413a79523f91f4946
Chris Brodt <chris@uberbrodt.net>
2015-11-19 15:22:32 -0600

    Added some signal handling code

--- example.go
+++ example.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"github.com/uberbrodt/signal_deadlock_example/Godeps/_workspace/src/github.com/uberbrodt/imagick/imagick"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"runtime"
+	"syscall"
+
+	"github.com/uberbrodt/signal_deadlock_example/Godeps/_workspace/src/github.com/uberbrodt/imagick/imagick"
 )
 
 func main() {
@@ -17,5 +21,19 @@ func main() {
 		log.Println("Hit the /test route")
 		imagick.Terminate()
 	})
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, syscall.SIGINT)
+	signal.Notify(sigint, syscall.SIGTERM)
+
+	go func() {
+		for {
+			<-sigint
+			log.Println("Caught SIGINT, shutting down imagick environment")
+			imagick.Terminate()
+			os.Exit(0)
+		}
+
+	}()
 	http.ListenAndServe(":4000", nil)
 }
